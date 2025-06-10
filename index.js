ac87d324dcac31a1dbb7c5de594678fc9480b7fb
Deekshitha Kashetty <87613821+DEEKSHITHA-K@users.noreply.github.com>
2025-06-10 19:34:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ initializeApp({
   credential: cert(serviceAccount)
 });
 const db = getFirestore();
+const serverless = require('serverless-http');
+
 
 var express = require('express')  
 var app = express()  
@@ -88,3 +90,5 @@ app.get('/contactSubmit', function (req, res) {
 app.listen(3000, function () {  
 console.log('Example app listening on port 3000!')  
 });
+
+module.exports.handler = serverless(app);
